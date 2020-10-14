244cfc7b27dfe0ed542c033c1244d3be91dfda95
hewittmcg <hewittmcg@users.noreply.github.com>
2020-10-14 17:01:29 -0400

    Wrote FW to toggle the onboard LED using the onboard button

--- Src/002led_button.c
+++ Src/002led_button.c
@@ -7,7 +7,7 @@ static void prv_delay(void) {
 }
 
 int main(void) { 
-    GPIO_handle_t gpio_led;
+    GPIO_handle_t gpio_led, button;
 
     // STM32F446RE onboard LED config
     gpio_led.p_GPIO_x = GPIOA;
@@ -17,17 +17,37 @@ int main(void) {
     gpio_led.pin_config.pin_op_type = GPIO_OP_TYPE_PP;
     gpio_led.pin_config.pin_pu_pd_control = GPIO_NO_PUPD;
 
+    // STM32F446RE onboard button config
+    button.p_GPIO_x = GPIOC;
+    button.pin_config.pin_number = GPIO_PIN_13;
+    button.pin_config.pin_mode = GPIO_MODE_IN;
+    button.pin_config.pin_speed = GPIO_SPEED_FAST;
+    button.pin_config.pin_pu_pd_control = GPIO_NO_PUPD;
+
     gpio_pclk_control(GPIOA, ENABLE);
     gpio_init(&gpio_led);
+
+    gpio_pclk_control(GPIOC, ENABLE);
+    gpio_init(&button);
+
+
     
     while(1) {
-        gpio_toggle_pin(GPIOA, GPIO_PIN_5);
-        prv_delay();
+    	// When pressed, pin pulls input low
+        if(!gpio_read_pin(GPIOC, GPIO_PIN_13)) {
+            prv_delay(); // delay to debounce
+            gpio_toggle_pin(GPIOA, GPIO_PIN_5);
+        }
+        
     }
 
     return 0;
 
 }
+
+
+
+
 
 
 
